0c0a7198dc0731828a16cdb11d707f5fba43cb55
0x186a0 <paul.0x186a0@gmail.com>
2024-06-06 19:09:16 +0300

    add: composer install

--- wsl-bootstrap.sh
+++ wsl-bootstrap.sh
@@ -8,3 +8,9 @@ sudo add-apt-repository -y ppa:ondrej/php
 
 sudo apt install -y php8.3
 sudo apt install -y php8.3-{xdebug,cli,pdo,mysql,pgsql,zip,gd,mbstring,curl,xml,bcmath,common}
+
+# Composer (php packet manager)
+
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
