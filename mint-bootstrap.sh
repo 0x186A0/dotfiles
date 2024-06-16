94e89655e3d06e12cffc6d9c93cb7a29e39ead6b
0x186a0 <paul.0x186a0@gmail.com>
2024-06-16 23:20:36 +0300

    add: php install

--- mint-bootstrap.sh
+++ mint-bootstrap.sh
@@ -1,6 +1,8 @@
-sudo apt update
+sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y git xclip
+sudo apt install curl gpg gnupg2 software-properties-common ca-certificates apt-transport-https lsb-release -y
+
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -39,9 +41,7 @@ sudo update-desktop-database
 cd ~/App
 
 # LED for Logitech keyboard
-
 sudo apt-get install  -y g++ make libhidapi-dev
-
 git clone https://github.com/MatMoul/g810-led.git
 cd g810-led
 make bin # for hidapi
@@ -49,12 +49,15 @@ make bin # for hidapi
 sudo make install
 
 # Neovim
-
-# Prerequisites
 sudo apt-get -y install ninja-build gettext cmake unzip curl
 git clone https://github.com/neovim/neovim
 cd neovim/
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 
+# PHP
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt update -y
+sudo apt -y install php8.3
+sudo apt install php8.3-{xdebug,cli,pdo,mysql,pgsql,zip,gd,mbstring,curl,xml,bcmath,common} -y
 
