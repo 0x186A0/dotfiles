65376745d21acba9ccaa49a3806ad3311bee56c5
0x186A0 <146991826+0x186A0@users.noreply.github.com>
2024-10-01 10:37:49 +0300

    Update wsl-bootstrap.sh

--- wsl-bootstrap.sh
+++ wsl-bootstrap.sh
@@ -1,16 +1,1 @@
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y git software-properties-common
-
-sudo add-apt-repository -y ppa:ondrej/php
-
-# PHP install
-
-sudo apt install -y php8.3
-sudo apt install -y php8.3-{xdebug,cli,pdo,mysql,pgsql,zip,gd,mbstring,curl,xml,bcmath,common}
-
-# Composer (php packet manager)
-
-sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-rm composer-setup.php
+echo Hello, World!!!
